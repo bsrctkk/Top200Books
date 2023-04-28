b50831ee3068bb203249c623b5c6458166ef1e91
bsrctkk <81588118+bsrctkk@users.noreply.github.com>
2023-04-28 20:50:31 +0300

    Add project files.

---
+++ Top200Books/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Top200Books
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCount = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 141);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 82;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(1335, 640);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(51, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Count:";
+            // 
+            // lblCount
+            // 
+            this.lblCount.AutoSize = true;
+            this.lblCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblCount.Location = new System.Drawing.Point(198, 78);
+            this.lblCount.Name = "lblCount";
+            this.lblCount.Size = new System.Drawing.Size(40, 42);
+            this.lblCount.TabIndex = 2;
+            this.lblCount.Text = "0";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSearch.Location = new System.Drawing.Point(682, 75);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(452, 49);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(1171, 75);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(188, 49);
+            this.btnSearch.TabIndex = 4;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1402, 807);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.lblCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCount;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ Top200Books/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Top200Books
+{
+    public partial class Form1 : Form
+    {
+        List<Book> books = new List<Book>();
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            WebClient wc = new WebClient();
+            string bookdata = wc.DownloadString("https://www.goodreads.com/list/show/18834.BBC_Top_200_Books/");
+            int contentStartIndex = bookdata.IndexOf("class=\"tableList js-dataTooltip\">");
+            int contentEndIndex = bookdata.IndexOf("</table");
+            string content = bookdata.Substring(contentStartIndex, contentEndIndex - contentStartIndex);
+            List<string> list = new List<string>();
+
+            while (content.Contains("<tr itemscope itemtype=\"http://schema.org/Book\">"))
+            {
+                int trStartIndex = content.IndexOf("<tr itemscope itemtype=\"http://schema.org/Book\">");
+                int trEndIndex = content.IndexOf("</tr>");
+
+                string bookContent = content.Substring(trStartIndex, trEndIndex - trStartIndex);
+
+                list.Add(bookContent);
+
+                content = content.Substring(trEndIndex + 6);
+            }
+
+            foreach (string str in list)
+            {
+                Book book = new Book();
+                int nameIndex = str.IndexOf("aria-level=");
+                int nameEndIndex = str.IndexOf("</span>");
+
+                book.name = str.Substring(nameIndex + 15, nameEndIndex - nameIndex - 15);
+
+                books.Add(book);
+            }
+            dataGridView1.DataSource = books;
+            lblCount.Text = books.Count.ToString(); 
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //dataGridView1.DataSource = books;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searcText=txtSearch.Text.ToLower().Trim();
+            var data=books.Where(x => x.name.ToLower().Contains(searcText)).ToList();
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource= data;
+            lblCount.Text= data.Count.ToString();
+        }
+    }
+}
